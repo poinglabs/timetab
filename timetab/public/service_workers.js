d0ea8d52ea4069a9254e418f0c9f0ba4b963142e
Mathias Unger <matiunger@gmail.com>
2021-05-02 15:42:45 -0300

    update git ignore

--- timetab/public/service_workers.js
+++
@@ -1,9 +0,0 @@
-self.addEventListener('message', function (event) {
-    if (event.data.event !== 'notification') return;
-    var options = {
-        body: event.data.message,
-        icon: "/timetab32.png"
-    };
-    console.log(event.data.message)
-    event.waitUntil(self.registration.showNotification(event.data.title, options));
-}); 
--- timetab/src/components/analytics.js
+++
@@ -1,7 +0,0 @@
-export const logEvent = (event_name, parameters) => {
-   let payload = parameters
-   payload.event = event_name
-   //window.dataLayer = window.dataLayer || []
-   //window.dataLayer.push(payload)
-   return;
-};
