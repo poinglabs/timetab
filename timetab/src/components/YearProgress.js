d574ea8449d0438547c951f3177a937353b5ba98
Mathias Unger <matiunger@gmail.com>
2023-04-17 19:43:41 -0300

    init seasons

--- timetab/src/components/YearProgress.js
+++ timetab/src/components/YearProgress.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import '../css/YearProgress.css';
 import { useTranslation } from 'react-i18next';
+import WbSunnyIcon from '@material-ui/icons/WbSunny';
+import AcUnitIcon from '@material-ui/icons/AcUnit';
+import LocalFloristIcon from '@material-ui/icons/LocalFlorist';
+import EcoIcon from '@material-ui/icons/Eco';
+
 
 function YearProgress(props) {
 
@@ -31,20 +36,34 @@ function YearProgress(props) {
     <div style={props.style} id="year-progress">
       <div className="year-container__title">{year}</div>
       <div className="year-container">
-        <div className="year-container__progress-bar" style={{width:progress+"%"}}></div>
-        <div className="year-container__year">
-          <div className="year-container__month year-container__month--first">{formatMonth(0)}</div>
-          <div className="year-container__month">{formatMonth(1)}</div>
-          <div className="year-container__month">{formatMonth(2)}</div>
-          <div className="year-container__month">{formatMonth(3)}</div>
-          <div className="year-container__month">{formatMonth(4)}</div>
-          <div className="year-container__month">{formatMonth(5)}</div>
-          <div className="year-container__month">{formatMonth(6)}</div>
-          <div className="year-container__month">{formatMonth(7)}</div>
-          <div className="year-container__month">{formatMonth(8)}</div>
-          <div className="year-container__month">{formatMonth(9)}</div>
-          <div className="year-container__month">{formatMonth(10)}</div>
-          <div className="year-container__month year-container__month--last">{formatMonth(11)}</div>
+        <div className="year-container_seasons-logos">
+          <div className="year-container__seasons-logo seasons-logo-1"><EcoIcon /></div>
+          <div className="year-container__seasons-logo seasons-logo-2"><AcUnitIcon /></div>
+          <div className="year-container__seasons-logo seasons-logo-3"><LocalFloristIcon /></div>
+          <div className="year-container__seasons-logo seasons-logo-4"><WbSunnyIcon /></div>
+        </div>
+        <div className="year-container__seasons-lines">
+          <div className="year-container__seasons-line seasons-line-1"></div>
+          <div className="year-container__seasons-line seasons-line-2"></div>
+          <div className="year-container__seasons-line seasons-line-3"></div>
+          <div className="year-container__seasons-line seasons-line-4"></div>
+        </div>
+        <div className="year-container__bar">
+          <div className="year-container__progress-bar" style={{width:progress+"%"}}></div>
+          <div className="year-container__year">
+            <div className="year-container__month year-container__month--first">{formatMonth(0)}</div>
+            <div className="year-container__month">{formatMonth(1)}</div>
+            <div className="year-container__month">{formatMonth(2)}</div>
+            <div className="year-container__month">{formatMonth(3)}</div>
+            <div className="year-container__month">{formatMonth(4)}</div>
+            <div className="year-container__month">{formatMonth(5)}</div>
+            <div className="year-container__month">{formatMonth(6)}</div>
+            <div className="year-container__month">{formatMonth(7)}</div>
+            <div className="year-container__month">{formatMonth(8)}</div>
+            <div className="year-container__month">{formatMonth(9)}</div>
+            <div className="year-container__month">{formatMonth(10)}</div>
+            <div className="year-container__month year-container__month--last">{formatMonth(11)}</div>
+          </div>
         </div>
       </div>
       <div className="year-container__footer">
